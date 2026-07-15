575761c1714a3bb370091b5a1eca490e2c9ede2c
RAKSHIT1998 <84240184+RAKSHIT1998@users.noreply.github.com>
2026-07-15 19:00:20 +0530

    ls

--- apps/api/src/scripts/update-medicine-images.ts
+++ apps/api/src/scripts/update-medicine-images.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { connectDatabase, disconnectDatabase } from '../config/db';
 import { env } from '../config/env';
 import { logger } from '../config/logger';
